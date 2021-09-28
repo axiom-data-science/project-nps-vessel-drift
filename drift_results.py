52c29745786288e8543522d240c4ac6d585de409
Jesse Lopez <jesse@axds.co>
2021-09-28 12:15:11 -0700

    update docstrings for drift_results

--- drift_results.py
+++ drift_results.py
@@ -1,7 +1,7 @@
 # Container for drift result simulations
 import calendar
-from collections import defaultdict
 import datetime
+from collections import defaultdict
 from pathlib import Path
 from typing import Union
 
@@ -10,9 +10,9 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
+import utils
 from ais import AIS
 from esi import ESI
-import utils
 
 
 class DriftResult:
@@ -22,34 +22,72 @@ class DriftResult:
     Parameters
     ----------
     path: Path
-        Path to result file
+        Path to result file.
     ais: AIS
         AIS data container object.
     esi: ESI
         ESI data container object.
     prob_drift: float
-        Probability of vessel drifting based on Vessels of Concern and AIS data from 2015-2019.
+        Probability of vessel drifting based on Vessels of Concern and AIS data from
+        2015-2019.
 
     Attributes
     ----------
-    path: Path
-        Path to simulation result file.
-    start_date: datetime.date
-        Date of the start of the simulation.
-    data: pandas.DataFrame
-        DataFrame of pt, pb, stranding_hazard, esi_id, and region indexed by particle number.
+    path: Path Path to simulation result file. start_date: datetime.date Date of the
+        start of the simulation. data: pandas.DataFrame DataFrame of `pt`, `pb`,
+        `stranding_hazard`, `esi_id`, and `region` indexed by particle number.
+
+    Notes
+    -----
+    The terms `pt`, `pb`, and `stranding_hazard` are used to estimate the probability of
+    a vessel drifting and becoming stranded on a particular section of interest along a
+    coastline using the methodology adopted from [1]_, [2]_.
+
+    `pt` is the probability that a vessel was at the release point based on AIS data.
+
+    `pb` is ratio of vessels that drifted and stranded on an ESI segment to the total
+    number of vessels for the simulation.
+
+    `stranding_hazard` is the probability that a vessel drifted and stranded based on
+    vessels of concern and AIS data from 2015-2019.
+
+    Methods with names including `calc_` include actual calculations whereas `get_`
+    indicates data subsetting.
+
+    ..[1] Sepp Neves, A.A., Pinardi, N. & Martins, F. IT-OSRA: applying ensemble
+    simulations to estimate the oil spill risk associated to operational and accidental
+    oil spills. Ocean Dynamics 66, 939–954 (2016).
+    https://doi.org/10.1007/s10236-016-0960-0
+
+    ..[2] Sepp Neves, A.A., Pinardi, N., Martins F., Janeiro, J., Samaras, A., Zodiatis,
+    G., De Dominicis, M., Towards a common oil spill risk assessment framework –
+    Adapting ISO 31000 and addressing uncertainties, Journal of Environmental
+    Management,Volume 159, 2015, Pages 158-168, ISSN 0301-4797,
+    https://doi.org/10.1016/j.jenvman.2015.04.044.
     """
     def __init__(self, path: Union[Path, str], ais: AIS, esi: ESI, prob_drift: float = 0.0005, **kwargs):
         self.path = Path(path)
         self.start_date = self._get_sim_starting_date()
         self.data = self._calc_drift_hazard(ais, esi, prob_drift, **kwargs)
 
-    def to_parquet(self, path: Union[Path, str], **kwargs):
-        """Write drift result to parquet file."""
+    def to_parquet(self, path: Union[Path, str], **kwargs) -> None:
+        """Write drift result to parquet file.
+        
+        Parameters
+        ----------
+        path: Path
+            Path to write parquet file to.
+        """
         self.data.to_parquet(path, **kwargs)
 
     def _get_sim_starting_date(self) -> datetime.date:
-        """Return simulation starting time as datetime.date"""
+        """Return simulation start date.
+
+        Returns
+        -------
+        start_date: datetime.date
+            Simulation start date.
+        """
         # Use first time step in file to ensure we get the correct starting time
         with xr.open_dataset(self.path) as ds:
             date = ds.time[0].dt.date.data.item()
@@ -57,7 +95,22 @@ class DriftResult:
         return date
 
     def _calc_drift_hazard(self, ais: AIS, esi: ESI, prob_drift: float, **kwargs) -> pd.DataFrame:
-        """Return drift hazard for each particle."""
+        """Return drift hazard for each particle.
+        
+        Parameters
+        ----------
+        ais: AIS
+            AIS data container object.
+        esi: ESI
+            ESI data container object.
+        prob_drift: float
+            Probability of vessel drifting.
+
+        Returns
+        -------
+        drift_hazard: pandas.DataFrame
+            Terms and regions associated with drift hazard calculations on a per particle basis.
+        """
         # Probability of vessel at release point r (Pt_r)
         pt = self._calc_pt_per_particle(ais, **kwargs)
 
@@ -82,7 +135,7 @@ class DriftResult:
                 region_per_particle.append(particle_esi_id.split('-')[0])
 
         # Return all factors and stranding risk in single DataFrame
-        return pd.DataFrame(
+        df = pd.DataFrame(
             {
                 'pt': pt,
                 'pb': pb,
@@ -92,12 +145,24 @@ class DriftResult:
             },
             index=np.arange(len(pt))
         )
+        df.attrs.update({'start_date': self.start_date})
 
-    def _get_starting_points(self, crs: str = 'epsg:4326', convert_lon: bool = True) -> pd.DataFrame:
-        """Return starting points for simulation as GeoDataFrame.
+        return df
 
-        Notes:
-        - Converts lon from [0, 360] to [-180, 180] if convert_lon is True
+    def _get_starting_points(self, crs: str = 'epsg:4326', convert_lon: bool = True) -> gpd.GeoDataFrame:
+        """Return drifting vessel starting points from simulation.
+
+        Parameters
+        ----------
+        crs: str
+            Coordinate reference system of simulation output. Typically 'epsg:4326'.
+        convert_lon: bool
+            Convert longitude values from 0 to 360 to -180 to 180.
+
+        Returns
+        -------
+        starting_points: geopandas.GeoDataFrame
+            GeoDataFrame of starting points for each particle in simulation indexed by particle number.
         """
         with xr.open_dataset(self.path) as ds:
             ds0 = ds.isel(time=0)
@@ -116,7 +181,18 @@ class DriftResult:
         return gdf.set_crs(crs)
 
     def _calc_pt_per_particle(self, ais: AIS, **kwargs) -> np.ndarray:
-        """Return Pt_r (probability of vessel at release point r) for each particle."""
+        """Return probability of vessel at release point at start of simulation (`pt`).
+
+        Parameters
+        ----------
+        ais: AIS
+            AIS data container object.
+
+        Returns
+        -------
+        pt: np.ndarray
+            Probability of vessel at release point at start of simulation.
+        """ 
         # Get starting position of very particle (drifting vessel)
         starting_points = self._get_starting_points(**kwargs)
         locs = np.vstack((starting_points.lon.values, starting_points.lat.values)).T
@@ -134,7 +210,18 @@ class DriftResult:
         return pt
 
     def _get_stranded_per_esi_segment(self, esi: ESI, **kwargs) -> pd.DataFrame:
-        """Return stranded vessels per ESI segment s."""
+        """Return the number of stranded vessels per ESI segment.
+
+        Parameters
+        ----------
+        esi: ESI
+            ESI data container object.
+
+        Returns
+        -------
+        stranded_vessels_per_esi_segment: pandas.DataFrame
+            Number of stranded vessels per ESI segment.
+        """
         esi_ids = self._get_esi_per_particle(esi, **kwargs)
 
         counts = defaultdict(int)
@@ -146,7 +233,18 @@ class DriftResult:
         return pd.DataFrame({'nstranded': counts.values()}, index=counts.keys())
 
     def _calc_pb_per_esi_segment(self, esi: ESI, **kwargs) -> pd.DataFrame:
-        """Return Pb_s, probability vessel drifted and stranded on ESI segment s."""
+        """Return probability vessel drifted and stranded on coastline.
+
+        Parameters
+        ----------
+        esi: ESI
+            ESI data container object.
+
+        Returns
+        -------
+        pb: pandas.DataFrame
+            Probability of vessel drifting and stranded on coastline indexed by ESI segment.
+        """ 
         stranded_by_esi = self._get_stranded_per_esi_segment(esi, **kwargs)
         pb_s = stranded_by_esi / stranded_by_esi.sum()
         pb_s.rename(columns={'nstranded': 'pb_s'}, inplace=True)
@@ -154,7 +252,18 @@ class DriftResult:
         return pb_s
 
     def _calc_pb_per_particle(self, esi: ESI, **kwargs) -> np.ndarray:
-        """Return Pb per particle."""
+        """Return `pb` of ESI segment where vessel stranded. 
+
+        Parameters
+        ----------
+        esi: ESI
+            ESI data container object.
+
+        Returns
+        -------
+        pb: np.ndarray
+            `pb` of ESI segment where vessel stranded.
+        """
         # Array of segments IDs per particle ('' if not stranded)
         esi_per_particle = self._get_esi_per_particle(esi, **kwargs)
         # DataFrame of pb_s (indexed by esi_id)
@@ -174,7 +283,22 @@ class DriftResult:
         dtype: str = 'U15',
         convert_lon: bool = True
     ) -> np.ndarray:
-        """Return ESI segment for every vessel (only a segment ID for vessels that are stranded)."""
+        """Return ESI segment for each vessel.
+
+        Parameters
+        ----------
+        esi: ESI
+            ESI data container object.
+        dtype: str
+            Data type of ESI segment IDs. (Default: 'U15')
+        convert_lon: bool
+            Convert longitude values from 0 to 360 to -180 to 180. (Default: True)
+        
+        Returns
+        -------
+        esi_ids: np.ndarray
+            ESI segment for each vessel.
+        """
         with xr.open_dataset(self.path) as ds:
             stranded_flag = get_stranded_flag(ds)
             nvessels = len(ds.trajectory)
@@ -201,7 +325,23 @@ class DriftResult:
 
 
 def get_stranded_flag(ds: xr.Dataset) -> int:
-    """Given DriftResult xr.Dataset, find the flag indicating stranded (not static between simulations)."""
+    """Return flag indicating stranded vessel.
+
+    Parameters
+    ----------
+    ds: xr.Dataset
+        Dataset containing stranded vessel flag.
+
+    Returns
+    -------
+    stranded_flag: int
+        Stranded vessel flag.
+    
+    Notes
+    -----
+    Stranded vessel flag is the value of the `status` variable in the dataset and
+    it is not constant between simulations.
+    """
     flag_meanings = ds.status.flag_meanings.split(' ')
     for ix, flag_meaning in enumerate(flag_meanings):
         if flag_meaning == 'stranded':

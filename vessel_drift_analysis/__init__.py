6be5a0f65619f7e6dae5a4c946e30443d40a9be5
Jesse Lopez <jesse@axds.co>
2021-10-04 18:31:56 -0700

    add plot script

--- vessel_drift_analysis/__init__.py
+++ vessel_drift_analysis/__init__.py
@@ -2,4 +2,5 @@ from . import ais
 from . import drift_results
 from . import esi
 from . import grs
+from . import plot
 from . import utils
--- vessel_drift_analysis/utils.py
+++ vessel_drift_analysis/utils.py
@@ -1,7 +1,16 @@
 # Convenience functions for working with the data
 import numpy as np
+import xarray as xr
 
 
 def lon360_to_lon180(lon: np.ndarray) -> np.ndarray:
     """Given lon in [0, 360) range, return lon in [-180, 180)"""
     return np.mod(lon - 180, 360) - 180
+
+
+def get_stranded_flag_from_status(ds: xr.Dataset) -> int:
+    """Given Dataset of results, return int indicating stranded"""
+    flag_meanings = ds.status.flag_meanings.split()
+    for ix, flag in enumerate(flag_meanings):
+        if flag == "stranded":
+            return ix
